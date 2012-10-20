250b3320b4389bcbed93d4dd1fef88e624164f68
moririring <moriri.ring10@gmail.com>
2012-10-20 13:28:13 +0900

    マウスの動きに合わせて動作

--- project/Assets/PointDomino.cs
+++ project/Assets/PointDomino.cs
@@ -14,17 +14,39 @@ public class PointDomino : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		var screenPosition = Input.mousePosition;
-		screenPosition.z = Camera.main.transform.position.y;
-		//
+		//var screenPosition = Input.mousePosition;
+		//screenPosition.z = Camera.main.transform.position.y;
+
+//Camera.main.ScreenPointToRay		
+		
+		var hit = new RaycastHit();
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);		
+		if (Physics.Raycast(ray, out hit))
+		{
+			transform.position = new Vector3(hit.point.x, 3.0f, hit.point.z);
+			if (Input.GetButtonDown("Fire1"))
+			{
+				var obj = Instantiate(dominoPrefab, transform.position, transform.rotation) as GameObject;
+				
+				//DominoManager.DominoItem item = new DominoManager.DominoItem();
+				//item.GameObject = obj;
+				//DominoManager.Domino.Items.Add(item);
+				
+				//Debug.Log(obj);
+			}
+		}
+			
+/*		
+		
 		var wpos = Camera.main.ScreenToWorldPoint(screenPosition);
 		wpos.y = 3;
 		transform.position = wpos;
+		*/
 		//
-		if(Input.GetMouseButtonDown(0))
-		{
-			Instantiate(dominoPrefab,transform.position, transform.rotation);
-		}
-		
+		//if(Input.GetMouseButtonDown(0))
+		//{
+			//Instantiate(dominoPrefab,transform.position, transform.rotation);
+		//}
 	}
+	
 }
